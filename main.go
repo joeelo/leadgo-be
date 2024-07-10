15e67a0b67d9ef291aabb02b579a25b9909f9c15
joeelo <josephclorenzo@gmail.com>
2024-07-09 21:52:15 -0700

    add config files

---
+++ main.go
@@ -0,0 +1,26 @@
+// https://stackoverflow.com/questions/76175950/connect-to-mongodb-when-you-have-google-login-as-credentials-for-mongodb-atlas
+
+package main
+
+import (
+	"fiber-mongo-api/configs"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	//run database
+	configs.ConnectDB()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+	})
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+	})
+
+	app.Listen(":6000")
+}
