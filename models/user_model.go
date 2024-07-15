2ef6417261aec3f131009031202c2a2fbc6985e0
joeelo <josephclorenzo@gmail.com>
2024-07-14 21:49:16 -0700

    add structs

---
+++ models/user_model.go
@@ -0,0 +1,13 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id            primitive.ObjectID `json:"id,omitempty"`
+	Name          string             `json:"name,omitempty" validate:"required"`
+	Email         string             `json:"email,omitempty" validate:"required"`
+	Pronouns      string             `json:"pronouns,omitempty" validate:"required"`
+	IsAdmin       bool               `json:"isAdmin,omitempty"`
+	Grade         string             `json:"grade,omitempty" validate:"required"`
+	SavedPrograms []primitive.ObjectID
+}
